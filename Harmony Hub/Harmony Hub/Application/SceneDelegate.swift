cecf887e5bf6db5e0656ea434cf38ed29185de76
LitvinovichRoman <127106050+LitvinovichRoman@users.noreply.github.com>
2024-06-11 18:12:06 +0300

    Firebase is connected to the project
    Connected:
    - Authentication
    - Storage
    - Realtime Database

--- Harmony Hub/Harmony Hub/Application/SceneDelegate.swift
+++ Harmony Hub/Harmony Hub/Application/SceneDelegate.swift
@@ -15,12 +15,12 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
-        window?.makeKeyAndVisible()
         
         let viewController = UIViewController()
         viewController.view.backgroundColor = .cyan
-        window?.rootViewController = viewController
         
+        window?.rootViewController = viewController
+        window?.makeKeyAndVisible()
     }
 }
 
