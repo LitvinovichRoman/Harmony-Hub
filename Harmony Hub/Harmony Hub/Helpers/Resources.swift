fe7fda6216d3d4f5aac2801797d2a8e56aba6279
LitvinovichRoman <127106050+LitvinovichRoman@users.noreply.github.com>
2024-06-28 17:14:08 +0300

    Arch/resources: Update Resources.swift

--- Harmony Hub/Harmony Hub/Helpers/Resources.swift
+++ Harmony Hub/Harmony Hub/Helpers/Resources.swift
@@ -10,8 +10,9 @@ import UIKit
 enum Resources {
     enum Colors {
         enum TabBar {
-            static var active = UIColor(named: "active")
-            static var inactive = UIColor(named: "inactive")
+            static var active = #colorLiteral(red: 0.9647058824, green: 0.4274509804, blue: 0.3882352941, alpha: 1)
+            static var inactive = #colorLiteral(red: 0.5019607843, green: 0.5019607843, blue: 0.5019607843, alpha: 1)
+            static var barBackground = #colorLiteral(red: 0.9142437178, green: 0.9142437178, blue: 0.9142437178, alpha: 1)
         }
     }
     
@@ -23,20 +24,65 @@ enum Resources {
             static var profile = "Профиль"
         }
         
-        enum HomeScreen {
+        enum MainScreen {
             static var dayAdvice = "Совет дня"
             static var category = "Категори"
+            static var moreButtonTitle = "Еще"
+            static var categoryTitle = "Выберите категорию"
+            static var posesTitle = "Выберите позу"
+            static var poseScrinTittle = "Позы"
+        }
+        
+        enum Auth {
+            static var registrationTitle = "Давайте зарегистрируемся"
+            static var loginTitle = "Войдите в учетную запись"
+            static var loginSubTitle = "Добро пожаловать!"
+            static var emailPlaceholder = "Введите email"
+            static var passPlaceholder = "Введите пароль"
+            static var rePassPlaceholder = "Повторите пароль"
+            static var regButtonTitle = "Зарегистрироваться"
+            static var logButtonTitle = "Войти"
+            static var googleButtonTitle = "Войти через google"
+            static var regNowSubTitle = "Нет аккаунта?"
+            static var regNowButtonTitle = "Зарегистрироваться"
+        }
+        
+        enum ThoughtsScreen {
+            static var thoughtsScreenTitle = "Твои мысли"
+            static var addButtonTitle = "Записать мысль"
+        }
+        
+        enum BreathScreen {
+            static var breathScreenTitle = "Контроль дыхания"
+            static var startButtonTitle = "Начать"
+            static var stopButtonTitle = "Остановить"
+        }
+        
+        enum  profileScreen {
+            static var logo = "Harmony Hub"
+            static var logoutButtonTitle = "Выйти из профиля"
+            static var projectInfoButtonTitle = "О проекте"
         }
     }
-
+    
     enum Images {
         enum TabBar {
-            static var main = UIImage(named: "mainTab")
-            static var breath = UIImage(named: "breathTab")
-            static var thoughts = UIImage(named: "thoughtsTab")
-            static var profile = UIImage(named: "profileTab")
+            static var main = #imageLiteral(resourceName: "mainTab")
+            static var breath = #imageLiteral(resourceName: "breathTab")
+            static var thoughts = #imageLiteral(resourceName: "breathTab")
+            static var profile = #imageLiteral(resourceName: "profileTab")
+        }
+        
+        enum Background {
+            static var main = #imageLiteral(resourceName: "mainBg")
+            static var breath = #imageLiteral(resourceName: "breathBg")
+            static var thoughts = #imageLiteral(resourceName: "thoughtsBg")
+            static var profile = #imageLiteral(resourceName: "profileBg")
+        }
+        
+        enum Thoughts {
+            static var newThought = #imageLiteral(resourceName: "newThought")
         }
     }
-
 }
 
