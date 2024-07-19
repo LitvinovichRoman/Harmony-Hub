383d5b60dbb9a9af9d89817f2ba472aaa1dbe16f
LitvinovichRoman <127106050+LitvinovichRoman@users.noreply.github.com>
2024-07-19 18:55:22 +0300

    Arch/resources: Update resources

---
+++ Harmony Hub/Harmony Hub/Supporting Files/Resources.swift
@@ -0,0 +1,102 @@
+//
+//  Resources.swift
+//  Harmony Hub
+//
+//  Created by Roman Litvinovich on 20.06.24.
+//
+
+import UIKit
+
+enum Resources {
+    //MARK: -- Resources[Strings]
+    enum Strings {
+        enum TabBar {
+            static var main: String = .localize("main")
+            static var breath: String = .localize("breath")
+            static var thoughts: String = .localize("thoughts")
+            static var profile: String = .localize("profile")
+        }
+        
+        enum MainScreen {
+            static var dayAdvice: String = .localize("dayAdvice")
+            static var category: String = .localize("category")
+            static var moreButtonTitle: String = .localize("moreButtonTitle")
+            static var categoryTitle: String = .localize("categoryTitle")
+            static var posesTitle: String = .localize("posesTitle")
+            static var poseScreenTittle: String = .localize("poseScreenTittle")
+        }
+        
+        enum Auth {
+            static var registrationTitle: String = .localize("registrationTitle")
+            static var loginTitle: String = .localize("loginTitle")
+            static var namePlaceholder: String = .localize("namePlaceholder")
+            static var emailPlaceholder: String = .localize("emailPlaceholder")
+            static var passPlaceholder: String = .localize("passPlaceholder")
+            static var regButtonTitle: String = .localize("regButtonTitle")
+            static var logButtonTitle: String = .localize("logButtonTitle")
+        }
+        
+        enum ThoughtsScreen {
+            static var thoughtsScreenTitle: String = .localize("thoughtsScreenTitle")
+            static var addButtonTitle: String = .localize("addButtonTitle")
+        }
+        
+        enum BreathScreen {
+            static var breathScreenTitle: String = .localize("breathScreenTitle")
+            static var startButtonTitle: String = .localize("startButtonTitle")
+            static var stopButtonTitle: String = .localize("stopButtonTitle")
+        }
+        
+        enum ProfileScreen {
+            static var logo: String = .localize("logo") }
+        static var logoutButtonTitle: String = .localize("logoutButtonTitle")
+        static var projectInfoButtonTitle: String = .localize("projectInfoButtonTitle")
+    }
+    
+    //MARK: -- Resources[Backgrounds]
+    enum Backgrounds {
+        static var main = #imageLiteral(resourceName: "mainBg")
+        static var breath = #imageLiteral(resourceName: "breathBg")
+        static var thoughts = #imageLiteral(resourceName: "thoughtsBg")
+        static var profile = #imageLiteral(resourceName: "profileBg")
+        static var login = #imageLiteral(resourceName: "loginViewBg")
+        static var registration = #imageLiteral(resourceName: "registrationViewBg")
+    }
+    
+    //MARK: -- Resources[Icons]
+    enum Icons {
+        enum TabBar {
+            static var main = #imageLiteral(resourceName: "mainTab")
+            static var breath = #imageLiteral(resourceName: "breathTab")
+            static var thoughts = #imageLiteral(resourceName: "breathTab")
+            static var profile = #imageLiteral(resourceName: "profileTab")
+        }
+        
+        enum Thoughts {
+            static var newThought = #imageLiteral(resourceName: "newThought")
+        }
+        
+        enum Auth {
+            static var googleButton = #imageLiteral(resourceName: "googleButton")
+        }
+    }
+    
+    //MARK: -- Resources[Colors]
+    enum Colors {
+        enum TabBar {
+            static var active = #colorLiteral(red: 0.631372549, green: 0.4666666667, blue: 0.4431372549, alpha: 1)
+            static var inactive = #colorLiteral(red: 0.5019607843, green: 0.5019607843, blue: 0.5019607843, alpha: 1)
+            static var barBackgroundColor = #colorLiteral(red: 0.9142437178, green: 0.9142437178, blue: 0.9142437178, alpha: 1)
+        }
+        
+        enum Auth {
+            static var titleLabelColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+            static var bottomButtonTitleColor = #colorLiteral(red: 0.5019607843, green: 0.5019607843, blue: 0.5019607843, alpha: 1)
+            static var textFieldBackgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+            static var textFieldTextColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+            static var mainButtonBackgroundColor = #colorLiteral(red: 0.631372549, green: 0.4666666667, blue: 0.4431372549, alpha: 1)
+            static var mainButtonTitleColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        }
+    }
+}
+
