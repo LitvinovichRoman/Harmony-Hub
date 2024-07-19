817ddf24cf40488e10818dac71b753a502978f04
LitvinovichRoman <127106050+LitvinovichRoman@users.noreply.github.com>
2024-07-19 19:08:56 +0300

    App/Features: Added Builder class
    - Implemented assembling a finished views

---
+++ Harmony Hub/Harmony Hub/Builder/Builder.swift
@@ -0,0 +1,60 @@
+//
+//  Builder.swift
+//  Harmony Hub
+//
+//  Created by Roman Litvinovich on 16.07.24.
+//
+
+import UIKit
+
+class Builder {
+    static func getLoginView() -> UIViewController {
+        let view = LoginView()
+        let presenter = LoginViewPresenter(view: view)
+        view.presenter = presenter
+        return view
+    }
+    
+    static func getRegistrationView() -> UIViewController {
+        let view = RegistrationView()
+        let presenter = RegistrationViewPresenter(view: view)
+        view.presenter = presenter
+        return view
+    }
+    
+    static func getTabBar() -> UIViewController {
+        let view = TabBar()
+        let presenter = TabBarPresenter(view: view)
+        view.presenter = presenter
+        return view
+    }
+    
+    static func getMainView() -> UIViewController {
+        let view = MainView()
+        let presenter = MainViewPresenter(view: view)
+        view.presenter = presenter
+        return view
+    }
+    
+    static func getBreathView() -> UIViewController {
+        let view = BreathView()
+        let presenter = BreathViewPresenter(view: view)
+        view.presenter = presenter
+        return view
+    }
+    
+    static func getThoughtsView() -> UIViewController {
+        let view = ThoughtsView()
+        let presenter = ThoughtsViewPresenter(view: view)
+        view.presenter = presenter
+        return view
+    }
+    
+    static func getProfileView() -> UIViewController {
+        let view = ProfileView()
+        let presenter = ProfileViewPresenter(view: view)
+        view.presenter = presenter
+        return view
+    }
+    
+}
