8984434e2f220f8a74ed8fa6b3687bc204837d8c
LitvinovichRoman <127106050+LitvinovichRoman@users.noreply.github.com>
2024-10-17 19:25:41 +0300

    Modules/Profile: Update Profile Module
    - added test signOutButton button

--- Harmony Hub/Harmony Hub/Modules/App/Profile/Presenter/ProfileViewPresenter.swift
+++ Harmony Hub/Harmony Hub/Modules/App/Profile/Presenter/ProfileViewPresenter.swift
@@ -6,16 +6,29 @@
 //
 
 import UIKit
+
 // MARK: - ProfileViewPresenter Protocol
 protocol ProfileViewPresenterProtocol: AnyObject {
     init(view: ProfileViewProtocol)
+    var signOut: UIAction { get }
 }
 
 // MARK: - Final class ProfileViewPresenter
 final class ProfileViewPresenter: ProfileViewPresenterProtocol {
     weak var view: ProfileViewProtocol?
+    private let authManager = AuthManager()
     
-    required init(view: any ProfileViewProtocol) {
+    init(view: any ProfileViewProtocol) {
         self.view = view
     }
+     
+    lazy var signOut: UIAction = {
+        let action = UIAction { [ weak self ] _ in
+            self?.authManager.signOut()
+            NotificationCenter.default.post(name: .setRootViewController,
+                                            object: nil,
+                                            userInfo: [ Keys.viewController : StateCase.login ])
+        }
+        return action
+    }()
 }
--- Harmony Hub/Harmony Hub/Modules/App/Profile/View/ProfileView.swift
+++ Harmony Hub/Harmony Hub/Modules/App/Profile/View/ProfileView.swift
@@ -9,23 +9,45 @@ import UIKit
 
 protocol ProfileViewProtocol: AnyObject {}
 
-class ProfileView: UIViewController, Backgroundable, ProfileViewProtocol {
-    //MARK: - Presenter
+
+final class ProfileView: UIViewController, Backgroundable, ProfileViewProtocol {
+    //MARK: -- Properties
     var presenter: ProfileViewPresenterProtocol!
+    var authManager = AuthManager()
     
-    //MARK: - Properties
+    lazy var signOutButton: UIButton = {
+        $0.backgroundColor = .black
+        $0.setTitle("Exit", for: .normal)
+        $0.setTitleColor(.white, for: .normal)
+        $0.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        $0.widthAnchor.constraint(equalToConstant: 80).isActive = true
+        $0.layer.masksToBounds = true
+        $0.layer.cornerRadius = 20
+        $0.addAction(presenter.signOut, for: .touchUpInside)
+        return $0
+    }(UIButton())
     
-    
-    //MARK: - Lifecycle
+    //MARK: -- Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         makeBackground(image: Resources.Backgrounds.profile)
+        configureLayout()
     }
 }
 
 //MARK: - Configure
 private extension ProfileView {
+    func configureLayout() {
+        configureButton()
+    }
     
+    func configureButton() {
+        view.addSubviews(signOutButton)
+        NSLayoutConstraint.activate([
+            signOutButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            signOutButton.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+    }
 }
 
 
